f7410b13a13857e8d18c08c5bf5856db2ab5a05d
Minh Tri Lê <minh-tri.le@etu.utc.fr>
2016-10-06 00:47:05 +0200

    Rapport : Ex.1, q.1,2,3, 4 en cours; ex.2

--- tp1.lisp
+++ tp1.lisp
@@ -12,17 +12,21 @@
 
 (cadr (cdr (cdr (cdr '(DO RE MI FA SOL LA SI))))) ; SOL
 (CONS (cadr '((a b)(c d))) (cddr '(a (b (c))))) ; ((c d)) : construction de la liste avec l'élément liste (c d)
+							;cddr de (a (b (c)))=NIL
 (cons (cons 'hello nil) '(how are you)) ; ((hello) how are you)
-(cons 'je (cons 'je (cons 'je (cons 'balbute nil)))) ; (je (je (je (balbute))))
-(cadr (cons 'tiens (cons '(c est simple) ()))) ; (c est simple)
+	  ;cons 'hello nil=(hello)
+(cons 'je (cons 'je (cons 'je (cons 'balbute nil)))) ; (je (je (je (balbute)))) 
+							  ;cons 'balbute nil=(balbute)
+(cadr (cons 'tiens (cons '(c est simple) ()))) ; cadr de (tiens (c est simple))=(c est simple)
 
 ;Q.4
+;Est-ce que : (a (b c) d) -> (a a (b b c c) (b b c c) d d) ou (a a (b c) (b c) d d)??
 
 (defun double (L)
 (if (listp L)
 (if (< 0 (length L))
 (append (list (car L) (car L)) (double(cdr L)))
-(car L)
+;(car L)
 )
 (nil)
 )
@@ -55,3 +59,16 @@
     (t 0)
    )
 )
+
+
+
+
+
+
+
+;Ex.2
+(defun list-triple (LL)
+(mapcar #'(lambda (j) (* j 3)) LL)
+)
+
+
